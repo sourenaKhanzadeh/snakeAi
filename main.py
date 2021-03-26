ea132c2958e642dc0f4d129dcc6d38b1abe581fe
sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2021-03-26 16:01:38 -0400

    CREATE: Main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+from game import Snake
+from agent import Agent
+from setting import *
+
+
+class Game:
+    def __init__(self):
+        self.snakes = []
+        self.agents = []
+
+    def awake(self):
+        pass
+        
+    def run(self):
+        pass
+
+
+    def quit(self):
+        pass
+
+if __name__ == "__main__":
+    pass
