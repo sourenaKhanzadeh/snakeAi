a68b63399facb4935532d3e5ce4d319f2a88c393
sourena Khanzadeh <sourena.khanzadeh@gmail.com>
2021-03-26 18:13:50 -0400

    CREATE: multi parrallel processing sample created just for demo

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
 from game import Snake
 from agent import Agent
 from setting import *
+from enum imort Enum
+import os
+
+class Windows(Enum):
+    W1 = (12, 12, 5, 5)
+    W2 = (6, 6, 10, 10)
 
 
 class Game:
-    def __init__(self):
+    def __init__(self, lv=1):
         self.snakes = []
         self.agents = []
 
+        self.lv = lv
+
+
     def awake(self):
-        pass
-        
+        for window in Windows:
+            if window.name == "W" + str(self.lv):
+                n, m, k, l = window.value
+                n, m = (set_size(n), set_size(m))
+
+                for i in range(k):
+                    for j in range(l):
+                        game = Snake(n, m)
+                        self.snake.append((i, j, game))
+                        self.agent.append((i, j, Agent(game)))
+                break
+
+
+    def train(self):
+        record = 0
+        game = Snake()
+        agent = Agent(game)
+        while True:
+            # get old state
+            state_old = game.get_state()
+
+            # get move
+            final_move = agent.get_action(state_old)
+
+            # perform move and get new state
+            reward, done, score = game.play_step(final_move)
+            state_new = game.get_state()
+
+            # train short memory
+            agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+            # remember
+            agent.remember(state_old, final_move, reward, state_new, done)
+
+            if done:
+                # train long memory, plot result
+                game.reset()
+                agent.n_games += 1
+                agent.train_long_memory()
+
+                if score > record:
+                    record = score
+                    agent.model.save()
+
+                print('Game', agent.n_games, 'Score', score, 'Record:', record)
+
+
     def run(self):
         pass
 
---
+++ multipross_sample.py
@@ -0,0 +1,38 @@
+import pygame
+from pygame.locals import *
+import multiprocessing as mp
+from multiprocessing import Pool, Process
+# Just a sample multiprocessing pygame
+
+class Game:
+    def __init__(self):
+        self.scene = []
+        self.screen = pygame.display.set_mode((100, 100))
+
+
+    def update(self):
+        run = True
+        while run:
+            for ev in pygame.event.get():
+                if ev.type == QUIT:
+                    run = False
+
+            self.screen.fill((0xff,0xff,0xff))
+            pygame.display.update()
+
+def exec():
+    g = Game()
+    g.update()
+    pygame.quit()
+    quit()
+
+
+if __name__ == "__main__":
+    processes = []
+    for i in range(4):
+        p  = Process(target=exec)
+        p.start()
+        processes.append(p)
+
+    for i in range(len(processes)):
+        p.join()
