d435deab76dea86e0b2f8194b9ae0a9a355e40bc
popTheSmart <popthesmart@hotmail.com>
2021-04-13 16:12:48 -0400

    update build_graphs.py, add 2 graph png's

--- graphs/build_graphs.py
+++ graphs/build_graphs.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
    scoreList = []
    recordList = []
 
-   file = open('default_parameters_20x20grid.txt')
+   file = open('no_training_20x20grid.txt')
    for row in file:
       row = row.split(' ')
       scoreList.append(int(row[1]))
@@ -22,24 +22,36 @@ if __name__ == "__main__":
    file.close()
 
    #cumulativeScoreList = np.cumsum(scoreList)
+   #ax[1].plot(cumulativeScore)
+   #ax[1].set_ylabel("Cumul. Score", fontsize=10)
+
    windowSize = 20
    movingAverageList = moving_average(scoreList, windowSize)
 
+   # Create a 3x1 plot
    fig = plt.figure()
    fig, ax = plt.subplots(3, 1, sharex='col', sharey='row')
    
-   ax[0].set_title('Default Parameters 20x20 Grid')
+   # Title
+   ax[0].set_title('No Training (Random Policy) 20x20 Grid')
 
+   # Score plot
    ax[0].plot(scoreList)
    ax[0].set_ylabel("Score", fontsize=10)
+   #ax[0].set_ylim([0,100])   #y-axis for trained policies
+   ax[0].set_ylim([0, 5])    #y-axis for no policy
 
-   #ax[1].plot(cumulativeScore)
-   #ax[1].set_ylabel("Cumul. Score", fontsize=10)
+   # Moving average plot
    ax[1].plot(movingAverageList)
-   ax[1].set_ylabel("Score \nMov. Avg = " + str(windowSize), fontsize=10)
+   ax[1].set_ylabel("Score\n(%s Game MA)" % (str(windowSize)), fontsize=10)
+   #ax[1].set_ylim([0,60])    #y-axis for trained policies
+   ax[1].set_ylim([0, 5])    #y-axis for no policy
 
+   # Record plot
    ax[2].plot(recordList)
    ax[2].set_ylabel("Record", fontsize=10)
    ax[2].set_xlabel("Game Number", fontsize=10)
+   #ax[2].set_ylim([0,100])   #y-axis for trained policies
+   ax[2].set_ylim([0, 5])    #y-axis for no policy
 
    plt.show()
