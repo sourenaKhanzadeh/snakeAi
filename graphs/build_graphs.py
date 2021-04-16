131e3f627a2bd22e2e30e514e638a22da7833400
popTheSmart <popthesmart@hotmail.com>
2021-04-16 03:34:47 -0400

    add SCORED: 50, LOOP: -50, COLWALL: -50

--- graphs/build_graphs.py
+++ graphs/build_graphs.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
    for i in range(3):
       scoreList = []
       recordList = []
-      file = open(f'scored__20__far__10__close__15__veryfar__5__/scored__20__far__10__close__15__veryfar__5__{i}.txt')
+      file = open(f'scored__50__loop__-50__colwall__-50__/scored__50__loop__-50__colwall__-50__{i}.txt')
       j = 0
       for row in file:
          
@@ -54,8 +54,8 @@ if __name__ == "__main__":
    fig = plt.figure()
    fig, ax = plt.subplots(3, 1, sharex='col', sharey='row')
    
-   # Title scored__20__far__10__close__15__veryfar__5__
-   ax[0].set_title('SCORED: 20, CLOSE: 15, FAR: 10, VERYFAR: 5')
+   # Title 
+   ax[0].set_title('SCORED: 50, LOOP: -50, COLWALL: -50')
 
    # Score plot
    ax[0].plot(score)
