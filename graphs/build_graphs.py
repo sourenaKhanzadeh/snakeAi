f94cd51f29505f789c3f1597a807fbe830558005
Sourena Khanzadeh <soure@DESKTOP-6PVNFEF.(none)>
2021-04-16 04:12:47 -0400

    UPDATE: graph dir added

--- graphs/build_graphs.py
+++ graphs/build_graphs.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
    for i in range(3):
       scoreList = []
       recordList = []
-      file = open(f'dir_closer__5_away___5_{i}.txt')
+      file = open(f'dir_closer__1_away___1_{i}.txt')
       j = 0
       for row in file:
          
@@ -55,7 +55,7 @@ if __name__ == "__main__":
    fig, ax = plt.subplots(3, 1, sharex='col', sharey='row')
    
    # Title
-   ax[0].set_title('MV_closer: 5, Mv_away: -5')
+   ax[0].set_title('MV_closer: 1, Mv_away: -1')
 
    # Score plot
    ax[0].plot(score)
